ab68909b583960f8991e3ea6673aa1a9b57dc5bf
Alex Inkin <alexander@inkin.ru>
2021-02-10 12:15:55 +0300

    feat(bind): add plugin for Observable host binding (#11)

---
+++ projects/ng-event-plugins/src/plugins/bind.plugin.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {concat, defer, EMPTY, Observable} from 'rxjs';
+import {takeWhile} from 'rxjs/operators';
+import {AbstractEventPlugin} from './abstract.plugin';
+
+@Injectable()
+export class BindEventPlugin extends AbstractEventPlugin {
+    protected readonly modifier = '$';
+
+    addEventListener(
+        element: HTMLElement & Record<string, Observable<unknown>>,
+        event: string,
+    ): Function {
+        element[event] = EMPTY;
+
+        const method = this.getMethod(element, event);
+        const zone$ = this.manager.getZone().onStable;
+        const sub = concat(
+            zone$.pipe(takeWhile(() => element[event] === EMPTY)),
+            defer(() => element[event]),
+        ).subscribe(method);
+
+        return () => sub.unsubscribe();
+    }
+
+    private getMethod(
+        element: HTMLElement & Record<string, unknown>,
+        event: string,
+    ): (v: unknown) => void {
+        const [, key, value, unit = ''] = event.split('.');
+
+        if (event.endsWith('.attr')) {
+            return v => element.setAttribute(key, String(v));
+        }
+
+        if (key === 'class') {
+            return v => element.classList.toggle(value, !!v);
+        }
+
+        if (key === 'style') {
+            return v => element.style.setProperty(value, `${v}${unit}`);
+        }
+
+        return v => (element[key] = v);
+    }
+}
--- projects/ng-event-plugins/src/public-api.ts
+++ projects/ng-event-plugins/src/public-api.ts
@@ -3,6 +3,7 @@
  */
 export * from './constants/plugins';
 export * from './decorators/should-call';
+export * from './plugins/bind.plugin';
 export * from './plugins/capture.plugin';
 export * from './plugins/prevent.plugin';
 export * from './plugins/silent.plugin';
