e14a5b24dfc08b4eca6e663e39be2b334558c664
aspiring-open-source-dev <info@waterplea.com>
2021-05-27 19:36:47 +0300

    feat(module): add module with providers

--- projects/demo/src/app/app.browser.module.ts
+++ projects/demo/src/app/app.browser.module.ts
@@ -1,43 +1,47 @@
-import {APP_BASE_HREF, LocationStrategy, PathLocationStrategy} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {NG_EVENT_PLUGINS} from '@tinkoff/ng-event-plugins';
-import {HIGHLIGHT_OPTIONS, HighlightModule} from 'ngx-highlightjs';
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app.routes';
-import {StaticModule} from './modules/static/static.module';
+import {
+  APP_BASE_HREF,
+  LocationStrategy,
+  PathLocationStrategy
+} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { EventPluginsModule } from '@tinkoff/ng-event-plugins';
+import { HIGHLIGHT_OPTIONS, HighlightModule } from 'ngx-highlightjs';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routes';
+import { StaticModule } from './modules/static/static.module';
 
 @NgModule({
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule.withServerTransition({appId: 'demo'}),
-        AppRoutingModule,
-        StaticModule,
-        HighlightModule,
-    ],
-    declarations: [AppComponent],
-    providers: [
-        {
-            provide: LocationStrategy,
-            useClass: PathLocationStrategy,
-        },
-        {
-            provide: APP_BASE_HREF,
-            useValue: '',
-        },
-        {
-            provide: HIGHLIGHT_OPTIONS,
-            useValue: {
-                coreLibraryLoader: () => import('highlight.js/lib/core'),
-                lineNumbersLoader: () => import('highlightjs-line-numbers.js'), // Optional, only if you want the line numbers
-                languages: {
-                    typescript: () => import('highlight.js/lib/languages/typescript'),
-                    less: () => import('highlight.js/lib/languages/less'),
-                    xml: () => import('highlight.js/lib/languages/xml'),
-                },
-            },
-        },
-        NG_EVENT_PLUGINS,
-    ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'demo' }),
+    EventPluginsModule,
+    AppRoutingModule,
+    StaticModule,
+    HighlightModule
+  ],
+  declarations: [AppComponent],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    },
+    {
+      provide: APP_BASE_HREF,
+      useValue: ''
+    },
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        coreLibraryLoader: () => import('highlight.js/lib/core'),
+        lineNumbersLoader: () => import('highlightjs-line-numbers.js'), // Optional, only if you want the line numbers
+        languages: {
+          typescript: () => import('highlight.js/lib/languages/typescript'),
+          less: () => import('highlight.js/lib/languages/less'),
+          xml: () => import('highlight.js/lib/languages/xml')
+        }
+      }
+    }
+  ]
 })
 export class AppBrowserModule {}
