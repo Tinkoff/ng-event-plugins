cd482e04041c8a6ab7ec721464a94258db339013
waterplea <alexander@inkin.ru>
2021-12-10 12:16:36 +0300

    feat: add web-types for autocomplete

--- projects/ng-event-plugins/src/decorators/should-call.ts
+++ projects/ng-event-plugins/src/decorators/should-call.ts
@@ -2,31 +2,34 @@ import {NgZone} from '@angular/core';
 import {Predicate} from '../types/predicate';
 
 /**
- * TODO: This will not be needed in Angular 10
- * when libraries are allowed to use Ivy renderer and markDirty becomes stable API
+ * TODO: This will not be needed when markDirty becomes stable API
  */
 export function shouldCall<T>(predicate: Predicate<T>): MethodDecorator {
     return (_, key, desc: PropertyDescriptor) => {
         const {value} = desc;
 
-        desc.value = function() {
-            const zone = arguments[0] as NgZone;
+        desc.value = function (this: T, ...args: any[]) {
+            const zone = arguments[0];
 
-            Object.defineProperty(this, key, {
-                value(this: T, ...args: any[]) {
-                    if (predicate.apply(this, args)) {
-                        zone.run(() => {
-                            value.apply(this, args);
-                        });
-                    }
-                },
-            });
+            if (zone instanceof NgZone) {
+                Object.defineProperty(this, key, {
+                    value(this: T, ...args: any[]) {
+                        if (predicate.apply(this, args)) {
+                            zone.run(() => {
+                                value.apply(this, args);
+                            });
+                        }
+                    },
+                });
+            } else if (predicate.apply(this, args)) {
+                value.apply(this, args);
+            }
         };
     };
 }
 
 /**
- * TODO: Use this in Angular 10
+ * TODO: Use this after markDirty becomes public API
  */
 // export function shouldCall<T extends object>(predicate: Predicate<T>): MethodDecorator {
 //     return (_: Object, _key: string | symbol, desc: PropertyDescriptor) => {
